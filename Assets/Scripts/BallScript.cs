e9905dd23f37cc4f9e2519058018d92bcb538f41
andchelsea <andchelsea@gmail.com>
2017-01-18 21:36:45 -0800

    Set up level
    Have scoring and kicking, ball and goal fully working. Kick takes power
    and angle and resets

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+    public bool Alive = true;
+	// Use this for initialization
+	void Awake () {
+     }
+	void ShooterScore()
+    {
+        Alive = false;
+        this.gameObject.SetActive(Alive);
+    }
+    void GoalieScore()
+    {
+        Alive = false;
+        this.gameObject.SetActive(Alive);
+    }
+	// Update is called once per frame
+	void Update () {
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Goal"))
+        {
+            ShooterScore();
+        }
+        if(collision.gameObject.CompareTag("Goalie"))
+        {
+            GoalieScore();
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("PlayArea"))
+        {
+            GoalieScore();
+        }
+    }
+}
